0df8aa09673c7bec5d2b570782d043fd35e782a1
eduviere israel <eduviereisrael@gmail.com>
2024-07-29 07:28:36 +0100

    second link

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,7 @@ export default function Home() {
           </div>
           <div className="buttons-container">
             <a href="/category" className="vote-button">Vote now</a>
-            <a className="tickets-button" href="https://wa.me/+2349080267234" >Buy Tickets</a>
+            <a className="tickets-button" target="_blank" href="https://wa.me/+2349080267234" >Buy Tickets</a>
           </div>
         </div>
 
